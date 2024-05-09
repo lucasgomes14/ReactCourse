f3c1cdd8151baa2f7207d75c595eaf27b39d7f5e
lucasgomes14 <lucazmatehus14@gmail.com>
2024-05-09 17:18:16 -0300

    first commit

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá mundo</h1>
+      <p>primeira aula de react</p>
+    </div>
+  );
+}
+
+export default App;
