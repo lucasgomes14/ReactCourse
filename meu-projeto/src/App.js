74a5279336fd7ecf31c2eee17f2e306879199445
lucasgomes14 <lucazmatehus14@gmail.com>
2024-05-09 18:05:35 -0300

    components

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const name = "Lucas"
@@ -18,7 +19,7 @@ function App() {
       <p>Olá, {newName}</p>
       <p>Soma: {sum(2, 1)}</p>
       <img src={url} alt="Minha imagem" />
-      <img src={url} alt="Minha imagem" />
+      <HelloWorld />
     </div>
   );
 }
