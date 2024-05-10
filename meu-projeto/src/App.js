32341263b09bc82cfab230f59fc95c77c7530ed0
lucasgomes14 <lucazmatehus14@gmail.com>
2024-05-10 20:41:22 -0300

    Adding CSS

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome = "Maria"
 
   return (
     <div className="App">
+      <Frase />
+      <Frase />
+      <h1>sasada</h1>
       <SayMyName nome="Lucas" />
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
