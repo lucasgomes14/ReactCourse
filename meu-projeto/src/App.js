cb610802807a1b747385b7c120cc7998fd898097
lucasgomes14 <lucazmatehus14@gmail.com>
2024-05-13 16:33:38 -0300

    List rendering

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
-  const nome = "Maria"
+
+  const meusItens = ["React", "View", "Angular"]
 
   return (
     <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional />
+      <h1>Renderização de listas</h1>
+      <OutraLista itens={meusItens} />
+      <OutraLista itens={[]} />
     </div>
   );
 }
